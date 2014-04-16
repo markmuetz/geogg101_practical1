bd3c278cad7e60c95d467cb32ea57460354cb1be
markmuetz <markmuetz@gmail.com>
2014-04-16 17:04:08 +0100

    Add in all the simulation files.
    Do some basic processing, get it so as river discharge data is displayed
    expt vs ctrl (calibration/validation output), and a diff is produced.
    Already quite informative.

--- practical2/project.py
+++ practical2/project.py
@@ -4,6 +4,7 @@ import datetime as dt
 import dateutil.parser
 import numpy as np
 import pylab as plt
+from glob import glob
 
 DATA_DIR = 'data_files'
 
@@ -34,6 +35,8 @@ def val():
     sim_borehole = np.loadtxt('%s/cal_val/cal_val_borehole_sim.txt'%(DATA_DIR), delimiter='\t', dtype=object)
     obs_borehole = np.loadtxt('%s/obs/Borehole_Records.txt'%(DATA_DIR), delimiter='\t', dtype=object)
 
+    sim_river = np.loadtxt('%s/cal_val/cal_val_river_flow_sim.txt'%(DATA_DIR), delimiter='\t', dtype=object)
+
     dts1  = np.array([dateutil.parser.parse(sim_borehole[i, 0]) for i in range(len(sim_borehole))])
     dts2 = np.array([dateutil.parser.parse(obs_borehole[i, 0]) for i in range(len(obs_borehole))])
 
@@ -56,14 +59,58 @@ def val():
 
 	plt.axvline(x=dt.datetime(1971, 1, 1), color='k')
 	plt.axvline(x=dt.datetime(1976, 1, 1), color='k')
-    #plt.show()
+    plt.show()
 
-    return dts1, dts2, sim_borehole, obs_borehole
+    return {'times1': dts1, 
+            'times2': dts2, 
+            'borehole': sim_borehole, 
+            'river': sim_river, 
+            'obs_borehole': obs_borehole
+            #'obs_river': obs_river
+            }
+
+def sim(args, ctrl):
+    sims = ('cc_low', 'cc_med', 'cc_hi', 'deforestation', 'reforestation', 'cc_hi_precip_only', 'cc_hi_evap_only')
+    res = {}
+    for sim in sims:
+        res[sim] = {}
+        print(sim)
+        river_discharge_filename = glob('%s/sim/%s/*M11.txt'%(DATA_DIR, sim))[0]
+        borehole_filename = glob('%s/sim/%s/*SZ.txt'%(DATA_DIR, sim))[0]
+
+        river_discharge = np.loadtxt(river_discharge_filename, skiprows=3, delimiter='\t', dtype=object)
+        borehole = np.loadtxt(borehole_filename, skiprows=3, delimiter='\t', dtype=object)
+
+        res[sim]['river'] = river_discharge
+        res[sim]['borehole'] = borehole
+
+        for i in range(len(river_discharge[:, 0])):
+            if river_discharge[i, 0] != ctrl['river'][i, 0]:
+                print('uh-oh %s'%str(river_discharge[i, 0]))
+
+        f = plt.figure(2)
+        plt.title('%s - ctrl'%(sim))
+        ax1 = plt.subplot('211')
+        plt.plot(river_discharge[:, 1].astype(float), 'b--', label='r1')
+        #plt.plot(river_discharge[:, 2].astype(float), 'g--', label='r2')
+
+        plt.plot(ctrl['river'][:, 1].astype(float), 'b-', label='r1')
+        #plt.plot(ctrl['river'][:, 2].astype(float), 'g-', label='r2')
+
+        ax2 = plt.subplot('212')
+        plt.plot(river_discharge[:, 1].astype(float) - ctrl['river'][:, 1].astype(float), 'b-', label='r1')
+        #plt.plot(river_discharge[:, 2].astype(float) - ctrl['river'][:, 2].astype(float), 'g-', label='r2')
+
+        plt.legend()
+        plt.show()
+
+    return res
 
 def create_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--cal', action='store_true')
     parser.add_argument('-v', '--val', action='store_true')
+    parser.add_argument('-s', '--sim', action='store_true')
     return parser.parse_args()
 
 def main(args):
@@ -72,6 +119,8 @@ def main(args):
 	res['cal'] = cal()
     if args.val:
 	res['val'] = val()
+    if args.sim:
+	res['sim'] = sim(args, res['val'])
     plt.show()
     return res
 
