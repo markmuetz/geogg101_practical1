2bcb49a7ea4f757fcdbc4dc746ea27dea318680a
markmuetz <markmuetz@gmail.com>
2014-04-24 15:27:08 +0100

    Add in a new series of graphs: CC river regimes.
    Modify args so no longer need -v on with -s and -r.

--- practical2/project.py
+++ practical2/project.py
@@ -30,10 +30,7 @@ def cal():
 
     return dts1, dts2, sim_borehole, obs_borehole
 
-def val():
-    start_date = dt.datetime(1970, 1, 1)
-    end_date = dt.datetime(1981, 1, 1)
-
+def load_val():
     sim_borehole = np.loadtxt('%s/cal_val/cal_val_borehole_sim.txt'%(DATA_DIR), delimiter='\t', dtype=object)
     obs_borehole = np.loadtxt('%s/obs/Borehole_Records.txt'%(DATA_DIR), delimiter='\t', dtype=object)
 
@@ -46,8 +43,31 @@ def val():
     sim_river_times = np.array([dateutil.parser.parse(sim_river[i, 0]) for i in range(len(sim_river))])
     obs_river_times = np.array([dateutil.parser.parse(obs_river[i, 0]) for i in range(len(obs_river))])
 
+    return {'sim_borehole_times': sim_borehole_times, 
+            'obs_borehole_times': obs_borehole_times, 
+            'sim_river_times': sim_river_times, 
+            'obs_river_times': obs_river_times, 
+            'sim_borehole': sim_borehole, 
+            'sim_river': sim_river, 
+            'obs_borehole': obs_borehole,
+            'obs_river': obs_river
+            }
+
+def plot_val(args, res):
+    sim_borehole_times = res['sim_borehole_times'] 
+    obs_borehole_times = res['obs_borehole_times'] 
+    sim_river_times = res['sim_river_times'] 
+    obs_river_times = res['obs_river_times'] 
+    sim_borehole = res['sim_borehole'] 
+    sim_river = res['sim_river'] 
+    obs_borehole = res['obs_borehole']
+    obs_river = res['obs_river']
+
     borehole_names = ('Borehole 5', 'Borehole 35', 'Borehole 37', 'Borehole 65')
     river_names = ('Karup', 'Hagebro')
+
+    start_date = dt.datetime(1970, 1, 1)
+    end_date = dt.datetime(1981, 1, 1)
 
     graph_settings = (
             ((62, 66), np.arange(62, 67, 1)),
@@ -127,7 +147,7 @@ def val():
 
     for i in range(2):
 	ax = plt.subplot(2, 1, i + 1)
-        ax.set_ylabel('%s\nDischarge (m/s)'%river_names[i])
+        ax.set_ylabel('%s\ndischarge (m/s)'%river_names[i])
 	if i == 0:
 	    plt.text(dt.datetime(1977, 1, 1), 17, 'Validation period')
 	    plt.text(dt.datetime(1972, 1, 1), 17, 'Calibration period')
@@ -173,8 +193,10 @@ def val():
 
             print('  %s: RMSE: %1.2f, NSE: %1.2f, R: %1.2f'%(river_names[i], rmse(x, y), nse(x, y), r_value))
 
-            line = [slope * x.min() + intercept, slope * x.max() + intercept]
-            plt.plot((x.min(), x.max()), line, 'b-', label='R = %1.2f'%r_value)
+            #line = [slope * x.min() + intercept, slope * x.max() + intercept]
+            #plt.plot((x.min(), x.max()), line, 'b-', label='R = %1.2f'%r_value)
+            line = [slope * graph_settings[i][0][0] + intercept, slope * graph_settings[i][0][1] + intercept]
+            plt.plot(graph_settings[i][0], line, 'b-', label='R = %1.2f'%r_value)
 
             plt.xlim(graph_settings[i][0])
             plt.ylim(graph_settings[i][0])
@@ -189,16 +211,6 @@ def val():
         plt.figure('River cal')
     #plt.show()
 
-    return {'sim_borehole_times': sim_borehole_times, 
-            'obs_borehole_times': obs_borehole_times, 
-            'sim_river_times': sim_river_times, 
-            'obs_river_times': obs_river_times, 
-            'sim_borehole': sim_borehole, 
-            'sim_river': sim_river, 
-            'obs_borehole': obs_borehole,
-            'obs_river': obs_river
-            }
-
 def rmse(obs, mod):
     '''Calc RMSE, will not work if len(obs) != len(mod)'''
     return np.sqrt(1./len(obs) * ((obs - mod)**2).sum())
@@ -208,10 +220,15 @@ def nse(obs, mod):
     obs_mean = obs.mean()
     return 1 - ((obs - mod)**2).sum() / ((obs - obs_mean)**2).sum()
 
-def sim(args, ctrl):
-    sims = ('cc_low', 'cc_med', 'cc_hi', 'deforestation', 'reforestation', 'cc_hi_precip_only', 'cc_hi_evap_only')
+def sim(args, ctrl, cc_only=True):
+    if cc_only:
+        sims = ('cc_low', 'cc_med', 'cc_hi')
+    else:
+        sims = ('cc_low', 'cc_med', 'cc_hi', 'cc_hi_precip_only', 'cc_hi_evap_only')
+
+    f = plt.figure('Effects of CC')
     res = {}
-    for sim in sims:
+    for sim_index, sim in enumerate(sims):
         res[sim] = {}
         print(sim)
         river_discharge_filename = glob('%s/sim/%s/*M11.txt'%(DATA_DIR, sim))[0]
@@ -223,40 +240,42 @@ def sim(args, ctrl):
         res[sim]['sim_river'] = river_discharge
         res[sim]['sim_borehole'] = borehole
 
-        for i in range(len(river_discharge[:, 0])):
-            if river_discharge[i, 0] != ctrl['sim_river'][i, 0]:
-                print('uh-oh %s'%str(river_discharge[i, 0]))
+        if False:
+            for i in range(len(river_discharge[:, 0])):
+                if river_discharge[i, 0] != ctrl['sim_river'][i, 0]:
+                    print('uh-oh %s'%str(river_discharge[i, 0]))
 
-        f = plt.figure(sim)
-        plt.title('%s - ctrl'%(sim))
-        ax1 = plt.subplot('211')
-        plt.plot(river_discharge[:, 1].astype(float), 'b--', label='r1')
-        plt.plot(river_discharge[:, 2].astype(float), 'g--', label='r2')
+            plt.title('%s - ctrl'%(sim))
 
-        plt.plot(ctrl['sim_river'][:, 1].astype(float), 'b-', label='r1')
-        plt.plot(ctrl['sim_river'][:, 2].astype(float), 'g-', label='r2')
+            ax = plt.subplot(6, 2, 2 + sim_index * 4)
+            plt.plot(ctrl['sim_river_times'], river_discharge[:, 1].astype(float) - ctrl['sim_river'][:, 1].astype(float), 'b-', label='r1')
 
-        ax2 = plt.subplot('212')
-        plt.plot(river_discharge[:, 1].astype(float) - ctrl['sim_river'][:, 1].astype(float), 'b-', label='r1')
-        plt.plot(river_discharge[:, 2].astype(float) - ctrl['sim_river'][:, 2].astype(float), 'g-', label='r2')
+            ax = plt.subplot(6, 2, 4 + sim_index * 4)
+            plt.plot(ctrl['sim_river_times'], river_discharge[:, 2].astype(float) - ctrl['sim_river'][:, 2].astype(float), 'g-', label='r2')
 
-        plt.legend()
+            plt.legend()
 
+        names = ('Low', 'Medium', 'High') 
+        cs = ('g', 'Orange', 'r')
         if args.regime:
-            scen_reg = regime(args, ctrl['sim_river_times'], river_discharge)
-            ctrl_reg = regime(args, ctrl['sim_river_times'], ctrl['sim_river'])
-            plot_regime(args, scen_reg, fgr_string=sim, loc='first', name='%s scenario'%sim)
-            plot_regime(args, ctrl_reg, fgr_string=sim, loc='second', name='control')
-        #plt.show()
+            for r in range(2):
+                ax = plt.subplot(2, 3, 1 + sim_index + 3 * r)
+                if r == 0:
+                    ax.set_title(names[sim_index])
 
-    if args.regime:
-        sim = 'comb'
-        hi_reg = regime(args, ctrl['sim_river_times'], res['cc_hi']['sim_river'])
-        hi_precip_scen_reg = regime(args, ctrl['sim_river_times'], res['cc_hi_precip_only']['sim_river'])
-        hi_evapor_scen_reg = regime(args, ctrl['sim_river_times'], res['cc_hi_evap_only']['sim_river'])
-        combined_reg = (hi_precip_scen_reg + hi_evapor_scen_reg) / 2
-        plot_regime(args, combined_reg, fgr_string=sim, loc='first', name='%s scenario'%sim)
-        plot_regime(args, hi_reg, fgr_string=sim, loc='second', name='%s scenario'%sim)
+                scen_reg = regime(args, ctrl['sim_river_times'], river_discharge)
+                ctrl_reg = regime(args, ctrl['sim_river_times'], ctrl['sim_river'])
+                plot_regime(args, scen_reg[r], loc='first', name=sim, ax=ax, r=r, c=cs[sim_index])
+                plot_regime(args, ctrl_reg[r], loc='second', name='calibration', ax=ax, r=r)
+
+#    if args.regime:
+#        sim = 'comb'
+#        hi_reg = regime(args, ctrl['sim_river_times'], res['cc_hi']['sim_river'])
+#        hi_precip_scen_reg = regime(args, ctrl['sim_river_times'], res['cc_hi_precip_only']['sim_river'])
+#        hi_evapor_scen_reg = regime(args, ctrl['sim_river_times'], res['cc_hi_evap_only']['sim_river'])
+#        combined_reg = (hi_precip_scen_reg + hi_evapor_scen_reg) / 2
+#        plot_regime(args, combined_reg, fgr_string=sim, loc='first', name='%s scenario'%sim)
+#        plot_regime(args, hi_reg, fgr_string=sim, loc='second', name='%s scenario'%sim)
 
     return res
 
@@ -292,50 +311,47 @@ def regime(args, times, river_flows):
 
     return monthly_total
 
-def plot_regime(args, monthly_total, fgr_string='ctrl', loc='only', name=''):
-    months = [calendar.month_name[i + 1][:3] for i in range(12)]
+def plot_regime(args, monthly_total, loc='only', name='', ax=None, r=0, c=''):
+    months = [calendar.month_name[i + 1][:1] for i in range(12)]
 
-    f = plt.figure('%s: River regime'%fgr_string)
     # Plot the regimes two rivers one on top of the other.
-    graph_settings = ((110, 250), (50, 100))
+    graph_settings = ((120, 230), (55, 85))
 
-    #graph_settings = ((110, 220), (50, 80))
-    for j in range(2):
-        ax = plt.subplot(2, 1, j + 1)
-        if True:
-            if args.bar:
-                if loc == 'only':
-                    plt.bar(range(12), monthly_total[j].mean(axis=1), width=1, color='b', 
-                            yerr=monthly_total[j].std(axis=1), error_kw=dict(ecolor='k'))
-                elif loc == 'first':
-                    plt.bar(np.arange(12) + 0.1, monthly_total[j].mean(axis=1), width=0.4, color='b', 
-                            yerr=monthly_total[j].std(axis=1), error_kw=dict(ecolor='k'))
-                elif loc == 'second':
-                    plt.bar(np.arange(12) + 0.5, monthly_total[j].mean(axis=1), width=0.4, color='g', 
-                            yerr=monthly_total[j].std(axis=1), error_kw=dict(ecolor='k'))
-            else:
-                if loc == 'only':
-                    plt.plot(np.arange(12) + 0.5, monthly_total[j].mean(axis=1), 'b-')
-                elif loc == 'first':
-                    plt.plot(np.arange(12) + 0.5, monthly_total[j].mean(axis=1), 'b-', label=name)
-                elif loc == 'second':
-                    plt.plot(np.arange(12) + 0.5, monthly_total[j].mean(axis=1), 'g--', label=name)
-                plt.legend()
-
-            ax.set_xticks(np.arange(12) + 1./2)
-            ax.set_xticklabels(months)
-            plt.ylim(graph_settings[j])
-            plt.xlim((0, 12))
-        if j == 0:
-            plt.ylabel('Karup average monthly\ndischarge (cumecs)')
+    if True:
+        if args.bar:
+            if loc == 'only':
+                plt.bar(range(12), monthly_total.mean(axis=1), width=1, color='b', 
+                        yerr=monthly_total.std(axis=1), error_kw=dict(ecolor='k'))
+            elif loc == 'first':
+                plt.bar(np.arange(12) + 0.1, monthly_total.mean(axis=1), width=0.4, color='b', 
+                        yerr=monthly_total.std(axis=1), error_kw=dict(ecolor='k'))
+            elif loc == 'second':
+                plt.bar(np.arange(12) + 0.5, monthly_total.mean(axis=1), width=0.4, color='g', 
+                        yerr=monthly_total.std(axis=1), error_kw=dict(ecolor='k'))
         else:
-            plt.ylabel('Hagebro average monthly\ndischarge (cumecs)')
+            if loc == 'only':
+                plt.plot(np.arange(12) + 0.5, monthly_total.mean(axis=1), 'b-')
+            elif loc == 'first':
+                plt.plot(np.arange(12) + 0.5, monthly_total.mean(axis=1), '-', color=c, label=name)
+            elif loc == 'second':
+                plt.plot(np.arange(12) + 0.5, monthly_total.mean(axis=1), 'k--', label=name)
 
-        if False:
-            # Plot each year's monthly discharge.
-            for y in range(10):
-                plt.plot(np.arange(12) + 1./2, monthly_total[j][:, y], label='y:%i'%(y + 1971))
-            #plt.legend()
+        if name in ['simulated', 'observed']:
+            plt.legend(loc='best')
+        ax.set_xticks(np.arange(12) + 1./2)
+        ax.set_xticklabels(months)
+        plt.ylim(graph_settings[r])
+        plt.xlim((0, 12))
+    if name in ['cc_low', 'observed'] and r == 0:
+        plt.ylabel('Karup average monthly\ndischarge (cumecs)')
+    elif name in ['cc_low', 'observed']:
+        plt.ylabel('Hagebro average monthly\ndischarge (cumecs)')
+
+    if False:
+        # Plot each year's monthly discharge.
+        for y in range(10):
+            plt.plot(np.arange(12) + 1./2, monthly_total[:, y], label='y:%i'%(y + 1971))
+        #plt.legend()
 
 
 def create_args():
@@ -349,17 +365,22 @@ def create_args():
 
 def main(args):
     res = {}
+    res['val'] = load_val()
+
     if args.cal:
 	res['cal'] = cal()
     if args.val:
-	res['val'] = val()
+	plot_val(args, res['val'])
     if args.sim:
 	res['sim'] = sim(args, res['val'])
     if args.regime:
 	res['sim_reg'] = regime(args, res['val']['sim_river_times'], res['val']['sim_river'])
 	res['obs_reg'] = regime(args, res['val']['obs_river_times'], res['val']['obs_river'])
-        plot_regime(args, res['sim_reg'], loc='first', name='simulated')
-        plot_regime(args, res['obs_reg'], loc='second', name='observed')
+        f = plt.figure('cal regime')
+        for r in range(2):
+            ax = plt.subplot(2, 1, r + 1)
+            plot_regime(args, res['obs_reg'][r], loc='first', name='observed', ax=ax, c='b', r=r)
+            plot_regime(args, res['sim_reg'][r], loc='second', name='simulated', ax=ax, c='k', r=r)
     plt.show()
     return res
 
